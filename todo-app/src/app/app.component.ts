acb2139f74cc1f75e3a8ea008b804ec73c9b55c8
ddang9390 <dnld1ng@yahoo.com>
2019-05-14 16:26:17 -0400

    Added styling and add functionality

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todoArray=[];
   title = 'todo-app';
+
+  addTodo(value){
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
 }
